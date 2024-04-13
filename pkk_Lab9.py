378abf0c271c455e6884d0ae42feccaec7c7b242
paapa-kusi <166747979+paapa-kusi@users.noreply.github.com>
2024-04-13 19:58:54 -0400

    even more encode()

--- pkk_Lab9.py
+++ pkk_Lab9.py
@@ -1,10 +1,10 @@
 def encode(password):
-    encoded_password = ''
+    encoded_password = '' # Empty string for new password
     for i in password:
         i = int(i)
-        i += 3
+        i += 3 # Shifts each digit by 3
         i = str(i)
         encoded_password += i
     return encoded_password
 
-print("Hello")
+
