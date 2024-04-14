d0a0e9714ef1536bb926cea4bd11a5b917921e78
paapa-kusi <166747979+paapa-kusi@users.noreply.github.com>
2024-04-13 20:35:06 -0400

    even more encode()

--- pkk_Lab9.py
+++ pkk_Lab9.py
@@ -14,4 +14,24 @@ def decode(encoded_password):
         decoded_password += decoded_char
     return decoded_password
 
+if __name__ == '__main__':
+    while True:
+        print()
+        print('Menu')
+        print('-------------')
+        print('1. Encode')
+        print('2. Decode')
+        print('3. Quit')
+        print()
+        option = input("Please enter an option: ")
+        if option == "1":
+            num = input("Please enter your password to encode: ")
+            num = encode(num)
+            num2 = num
+            print("Your password has been encoded and stored!")
+        if option == "2":
+            num2 = decode(num2)
+            print(f"The encoded password is {num}, and the original password is {num2}")
+        if option == "3":
+            break
 
