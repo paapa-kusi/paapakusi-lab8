f97a87b53969f64ddd1fafc947f170688ae7ec77
aaron-joshy <aaronpjoshy@gmail.com>
2024-04-14 22:18:15 -0400

    comments added to decode

--- pkk_Lab9.py
+++ pkk_Lab9.py
@@ -1,5 +1,5 @@
 def encode(password):
-    encoded_password = '' # Empty string for new password
+    encoded_password = ''  # Empty string for new password
     for i in password:
         i = int(i)
         i += 3 # Shifts each digit by 3
@@ -8,9 +8,9 @@ def encode(password):
     return encoded_password
 
 def decode(encoded_password):
-    decoded_password = ""
+    decoded_password = ''  # Empty for new passwords
     for char in encoded_password:
-        decoded_char = chr(ord(char)-3)
+        decoded_char = chr(ord(char)-3) # shifts digits back by 3
         decoded_password += decoded_char
     return decoded_password
 
